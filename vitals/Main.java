63313c6c8c9bcbce1a117dc9b2a45c9bbedc7140
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-07-23 17:26:11 +0530

    Update Main.java to reduce cyclomatic complexity
    used a single validation method that combines the checks into a single structure. This way, we can avoid multiple if-else statements that increase complexity. We'll also minimize branching by using a single return statement.

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,18 +1,40 @@
 package vitals;
 
 public class Main {
+
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
+        return validateBattery(temperature, soc, chargeRate);
+    }
+
+    static boolean validateBattery(float temperature, float soc, float chargeRate) {
+        if (isTemperatureOutOfRange(temperature)) {
             System.out.println("Temperature is out of range!");
             return false;
-        } else if(soc < 20 || soc > 80) {
+        }
+
+        if (isSocOutOfRange(soc)) {
             System.out.println("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        }
+
+        if (isChargeRateOutOfRange(chargeRate)) {
             System.out.println("Charge Rate is out of range!");
             return false;
         }
+
         return true;
+    }
+
+    static boolean isTemperatureOutOfRange(float temperature) {
+        return temperature < 0 || temperature > 45;
+    }
+
+    static boolean isSocOutOfRange(float soc) {
+        return soc < 20 || soc > 80;
+    }
+
+    static boolean isChargeRateOutOfRange(float chargeRate) {
+        return chargeRate > 0.8;
     }
 
     public static void main(String[] args) {
