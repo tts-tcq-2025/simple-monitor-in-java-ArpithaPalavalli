f59c561ba52852f7fa3a50991cba26cb07ecffb4
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-07-23 17:56:03 +0530

    Update Main.java 2nd version
    2nd version

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,45 +1,52 @@
 package vitals;
 
-public class Main {
+public class Main
+{
 
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        return validateBattery(temperature, soc, chargeRate);
+    static boolean batteryIsOk(float temperature, float soc, float chargeRate)
+    {
+        return validateBattery(temperature, soc, chargeRate);}
+}
+
+static boolean validateBattery(float temperature, float soc, float chargeRate)
+{
+    String validationMessage = validateBatteryParameters(temperature, soc, chargeRate);
+    if (validationMessage != null)
+    {
+        System.out.println(validationMessage);
+        return false;
+    }
+    return true;
+}
+
+static String validateBatteryParameters(float temperature, float soc, float chargeRate)
+{
+    String returnVal = null;
+    if (isTemperatureOutOfRange(temperature) || isSocOutOfRange(soc) || isChargeRateOutOfRange(chargeRate))
+    {
+        returnVal = "Battery not OK!!";
     }
 
-    static boolean validateBattery(float temperature, float soc, float chargeRate) {
-        if (isTemperatureOutOfRange(temperature)) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        }
+    return returnVal; // All parameters are valid
+}
+static boolean isTemperatureOutOfRange(float temperature)
+{
+    return temperature < 0 || temperature > 45;
+}
 
-        if (isSocOutOfRange(soc)) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        }
+static boolean isSocOutOfRange(float soc)
+{
+    return soc < 20 || soc > 80;
+}
 
-        if (isChargeRateOutOfRange(chargeRate)) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
+static boolean isChargeRateOutOfRange(float chargeRate)
+{
+    return chargeRate > 0.8;
+}
 
-        return true;
-    }
-
-    static boolean isTemperatureOutOfRange(float temperature) {
-        return temperature < 0 || temperature > 45;
-    }
-
-    static boolean isSocOutOfRange(float soc) {
-        return soc < 20 || soc > 80;
-    }
-
-    static boolean isChargeRateOutOfRange(float chargeRate) {
-        return chargeRate > 0.8;
-    }
-
-    public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
-    }
+public static void main(String[] args)
+{
+    assert(batteryIsOk(25, 70, 0.7f) == true);
+    assert(batteryIsOk(50, 85, 0.0f) == false);
+    System.out.println("Some more tests needed");
 }
